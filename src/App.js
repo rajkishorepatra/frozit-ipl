60052c1ff0c66247e57f1dc9383e0619c7d1f4c0
rajkishorepatra <rpatrasm@gmail.com>
2023-04-03 18:25:17 +0530

    New Changes

--- src/App.js
+++ src/App.js
@@ -1,115 +1,114 @@
 import './App.css';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import bgimg from './img/bg.jpg';
-import { Form, Nav } from 'react-bootstrap';
-import { Parallax, ParallaxProvider } from 'react-scroll-parallax';
+import { Form } from 'react-bootstrap';
 import Carousel from 'react-bootstrap/Carousel';
+import { useState, useRef } from 'react';
 
 function App() {
+
+  const [num, setNum] = useState('');
+  const formRef = useRef(null)
+  const scriptUrl = "https://script.google.com/macros/s/AKfycbzcPETc87OkAXgtSBFsJwTFhFqYh6I2zsQ6jQfEznmxuA_Wr1Nzg222cTWPdvvEO9Af/exec"
+  
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert('Thanks for Participating!😄');
+    fetch(scriptUrl, {
+      method: 'POST',
+      'mode': 'no-cors',
+      body: new FormData(formRef.current),
+
+    }).then(res => {
+      console.log("SUCCESSFULLY SUBMITTED")
+    })
+      .catch(err => console.log(err));
+    setNum('');
+  }
+
   return (
     <>
-      <Navbar collapseOnSelect fixed="top" expand="lg" variant="dark" style={{ width: '95%', borderRadius: '10px', top: '5px', zIndex: "100" }} className='m-auto'>
-        <Container>
-          <Navbar.Brand href="#home">Frozit.</Navbar.Brand>
-          <Nav style={{ color: 'white' }}>
-            <Nav.Link href="#campaigns" className='mx-2 text-white'>Campaigns</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-
-
       <div className='main' id='main'>
-        <Carousel>
+        <Carousel controls={false} indicators={false} fade>
           <Carousel.Item>
             <img
-              className="d-block w-100"
-              src={bgimg}
+              className="d-block w-100 imgSize"
+              src="https://ik.imagekit.io/0juyrwsxf/Frozit/Website-slide-1__1_-min.jpg?updatedAt=1680526029585"
               alt="First slide"
             />
-            <Carousel.Caption>
-              <h3>First slide label</h3>
-              <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100"
-              src={bgimg}
+              className="d-block w-100 imgSize"
+              src="https://ik.imagekit.io/0juyrwsxf/Frozit/Website-slide-2__1_-min.jpg?updatedAt=1680526030212"
               alt="Second slide"
             />
-
-            <Carousel.Caption>
-              <h3>Second slide label</h3>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-            </Carousel.Caption>
           </Carousel.Item>
           <Carousel.Item>
             <img
-              className="d-block w-100"
-              src={bgimg}
+              className="d-block w-100 imgSize"
+              src="https://ik.imagekit.io/0juyrwsxf/Frozit/Website-slide-3__1_-min.jpg?updatedAt=1680526030275"
               alt="Third slide"
             />
-
-            <Carousel.Caption>
-              <h3>Third slide label</h3>
-              <p>
-                Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-              </p>
-            </Carousel.Caption>
           </Carousel.Item>
         </Carousel>
       </div>
 
-      <ParallaxProvider>
-        <Parallax speed={-5}>
-          <section className="home-newsletter" id="testimonials">
-            <div className="container">
-              <div className="row">
-                <div className="col-sm-12">
-                  <div className="single">
-                    <h2>Participate in our Contest</h2>
-                    <Form>
-                      <div className="input-group">
-                        <input type="text" className="form-control" placeholder="Enter your Whatsapp number" />
-                        <span className="input-group-btn">
-                          <button
-                            className="btn btn-theme"
-                            type="submit"
-                            onClick={(e) => {
-                              e.preventDefault();
-                              alert('Thanks for Participating!😄');
-                            }}>
-                            Submit
-                          </button>
-                        </span>
-                      </div>
-                    </Form>
-                  </div>
+      <div style={{ backgroundColor: "#0053a0" }}>
+        <section className="home-newsletter" id="testimonials">
+          <div className="container">
+            <div className="row">
+              <div className="col-sm-12">
+                <div className="single">
+                  <h2>Participate in our Contest</h2>
+                  <Form onSubmit={handleSubmit} method='post' ref={formRef} name="google-sheet">
+                    <div className="input-group">
+                      <input
+                        type="text"
+                        className="form-control"
+                        placeholder="Enter your Whatsapp number"
+                        name='Whatsapp-Number'
+                        onChange={(e) => setNum(e.target.value)}
+                        value={num}
+                        required />
+                      <span className="input-group-btn">
+                        <button
+                          className="btn btn-theme"
+                          onClick={(e) => {
+                            if (e.target.value !== null) {
+                              e.target.style.backgroundColor = "#00963f"
+                            }
+                          }
+                          }
+                          type="submit"
+                        >
+                          Submit
+                        </button>
+                      </span>
+                    </div>
+                  </Form>
                 </div>
               </div>
             </div>
-          </section>
-
-
-          <div className='about text-white pb-3' id='about'>
-            <h1 className=' text-center'>Terms & Condition</h1>
-            <div className='aboutIns'>
-              <p>•&nbsp;&nbsp;Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              <p>•&nbsp;&nbsp;Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              <p>•&nbsp;&nbsp;Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              <p>•&nbsp;&nbsp;Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              <p>•&nbsp;&nbsp;Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              <p>•&nbsp;&nbsp;Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-              <p>•&nbsp;&nbsp;Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-            </div>
           </div>
-        </Parallax>
-      </ParallaxProvider>
+        </section>
 
-      <div className='footer text-center'>
-        {/* <p className='mx-2'>Copyright © 2022 <span>Raahgiri Foundation</span>. All Rights Reserved</p> */}
-        <p className='py-2'>In Partnership with <span>Fusion</span> (a unit of IAMPIX TECHNOLOGY PVT LTD).</p>
+        <div className='about text-white pb-1' id='about'>
+          <h1 className=' text-center text-uppercase'>How to play along ?</h1>
+          <div className='aboutIns align-items-left'>
+            <p>•&nbsp;&nbsp;Register with your phone number. </p>
+            <p>•&nbsp;&nbsp;Receive notification from our end.</p>
+            <p>•&nbsp;&nbsp;Play along and win everyday !</p>
+          </div>
+        </div>
+
+        <div className='logoImg m-4'>
+          <img src='https://ik.imagekit.io/0juyrwsxf/Frozit/logo-3-min.png?updatedAt=1680526028607' alt='logo' width={70} />
+          <img src='https://ik.imagekit.io/0juyrwsxf/Frozit/logo-2-min.png?updatedAt=1680526028647' alt='logo' width={70} />
+        </div>
+
+        <div className='footer text-center'>
+          <p className='py-2'>In Partnership with <span>Fusion</span> (a unit of IAMPIX TECHNOLOGY PVT LTD).</p>
+        </div>
+
       </div>
     </>
   );
